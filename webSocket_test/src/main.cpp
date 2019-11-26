38652d93a8a1f87c06d99e3c7a1edd475d12a577
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2019-11-26 08:49:00 +0100

    submodule test

--- webSocket_test/src/main.cpp
+++ webSocket_test/src/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
 
-#include <curlpp/cURLpp.hpp>
-#include <curlpp/Easy.hpp>
-#include <curlpp/Option.hpp>
+#include <curl/curl.h>
+#include <cpr/cpr.h>
 
-int main(int argc, char** argv)
-{
-    curlpp::Cleanup myCleanup;
-    std::ostringstream os;
-    os << curlpp::options::Url(std::string("http://www.wikipedia.org"));
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+int main(int argc, char** argv) {
+    auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
+                      cpr::Authentication{"user", "pass"},
+                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
+    r.status_code;                  // 200
+    r.header["content-type"];       // application/json; charset=utf-8
+    r.text;                         // JSON text string
 }
