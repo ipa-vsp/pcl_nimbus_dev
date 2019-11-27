d25514b017952e7885ddde215788f9c77c858912
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2019-11-27 10:31:48 +0100

    Failed test cpr
    failed attemtemts

--- webSocket_test/src/main.cpp
+++ webSocket_test/src/main.cpp
@@ -5,12 +5,20 @@
 
 #include <curl/curl.h>
 #include <cpr/cpr.h>
+#include <nlohmann/json.hpp>
+#include <assert.h>
 
 int main(int argc, char** argv) {
-    auto r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-                      cpr::Authentication{"user", "pass"},
-                      cpr::Parameters{{"anon", "true"}, {"key", "value"}});
-    r.status_code;                  // 200
-    r.header["content-type"];       // application/json; charset=utf-8
-    r.text;                         // JSON text string
+    auto url = cpr::Url{"http://192.168.0.69:8383/jsonrpc"};
+    auto header = cpr::Header{{"content-type", "application/json"}};
+    auto parameters = cpr::Parameters{{"hello", "world"}};
+    cpr::Session session;
+    session.SetUrl(url);
+    session.SetParameters(parameters);
+    session.SetHeader(header);
+
+    auto r = session.Post();             // Equivalent to cpr::Get(url, parameters);
+    std::cout << r.url << std::endl;     // Prints http://www.httpbin.org/get?hello=world
+
+
 }
